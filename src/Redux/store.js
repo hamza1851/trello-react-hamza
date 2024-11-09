2f4aba46663e4680ff752605d9659e35481f0c47
hamza1851 <hamza.dev011@gmail.com>
2024-11-09 17:24:39 +0530

    Add ChechListItems Slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit"
 import boardReducer from "../Features/Board/boardSlice"
 import listReducer from "../Features/List/listSlice"
 import listCardReducer from "../Features/List/listCardSlice"
+import checkListreducer from "../Features/CheckLists/checkListSlice"
+import checklistItemsReducer from "../Features/CheckLists/checkListItemSlice"
 
 const store = configureStore({
   reducer: {
     boards: boardReducer,
     lists: listReducer,
     listCards: listCardReducer,
+    checkLists: checkListreducer,
+    checklistItems: checklistItemsReducer,
   },
 })
 export default store
